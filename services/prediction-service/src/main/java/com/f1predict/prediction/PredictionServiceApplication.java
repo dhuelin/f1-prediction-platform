3f2617f82567055f6d49d3974ee66df45b9fa5ef
dhuelin <dhuelin@users.noreply.github.com>
2026-03-27 02:17:16 +0100

    [#52] Add Prediction Service project skeleton

---
+++ services/prediction-service/src/main/java/com/f1predict/prediction/PredictionServiceApplication.java
@@ -0,0 +1,11 @@
+package com.f1predict.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PredictionServiceApplication.class, args);
+    }
+}
